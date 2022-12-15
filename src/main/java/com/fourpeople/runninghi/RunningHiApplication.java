a51eaba20413227dc250f4a10451042ba1757ce4
seung-woo-ryu <tmddn645@naver.com>
2022-12-15 12:40:37 +0900

    first commit

---
+++ src/main/java/com/fourpeople/runninghi/RunningHiApplication.java
@@ -0,0 +1,13 @@
+package com.fourpeople.runninghi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningHiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourpeople/runninghi/RunningHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourpeople.runninghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningHiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
